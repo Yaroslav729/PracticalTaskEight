c66fb1557014ba41e6fd1621f5b0e877effd2b22
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-06-19 13:02:29 +0300

    fifty-four

---
+++ Task fifty-four/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+//  которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.Clear();
+int[,] matrix = new int[4, 7];
+
+void InputMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+                {
+                    if (matrix[i, k] < matrix[i, k + 1])
+                    {
+                        int sort = matrix[i, k + 1];
+                        matrix[i, k + 1] = matrix[i, k];
+                        matrix[i, k] = sort;
+                    }
+                }
+                }
+        }
+}
+
+void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                Console.Write($"{matrix[i, j]}\t");
+                Console.WriteLine();
+        }
+    }
+    InputMatrix(matrix);
+    PrintMatrix(matrix);
+    SortMatrix(matrix);
+    Console.WriteLine();
+    PrintMatrix(matrix);
